878d043f9168142ab9d956560590dc2f4dceb931
Ivo Spijkerman <ivo@spijkerman.nl>
2020-01-11 07:15:11 +0100

    made first things work

--- graphql/functions/src/index.ts
+++ graphql/functions/src/index.ts
@@ -4,7 +4,7 @@ import gqlServer from './graphql/server';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-export const api = functions.https.onRequest(gqlServer());
+export const api = functions.region('europe-west1').https.onRequest(gqlServer());
 
 export const helloWorld = functions.region('europe-west1').https.onRequest((request, response) => {
  response.send("Hello from Firebase!");
