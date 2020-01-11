c072adc1b73d8868061edbe64a8fba4ad6fd3f0c
Ivo Spijkerman <ivo@spijkerman.nl>
2020-01-11 06:46:55 +0100

    temp commit in unsuable state

---
+++ graphql/functions/src/index.ts
@@ -0,0 +1,11 @@
+import * as functions from 'firebase-functions';
+import gqlServer from './graphql/server';
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+
+export const api = functions.https.onRequest(gqlServer());
+
+export const helloWorld = functions.region('europe-west1').https.onRequest((request, response) => {
+ response.send("Hello from Firebase!");
+});
