4eba15cc3de97a7f1cbb04414070959a4a238e24
Ivo Spijkerman <ivo@spijkerman.nl>
2020-01-12 14:08:48 +0100

    added http denial

--- graphql/functions/src/graphql/server.ts
+++ graphql/functions/src/graphql/server.ts
@@ -1,11 +1,20 @@
 import express from 'express';
-import {ApolloServer} from 'apollo-server-express';
+import { ApolloServer } from 'apollo-server-express';
 
 import schema from './schema';
 import resolvers from './resolvers';
 
 function gqlServer() {
     const app = express();
+
+    app.enable('trust proxy');
+    app.use(function (req, res, next) {
+        if (req.secure) {
+            next();
+        } else {
+            res.status(497).send('Use HTTPS');
+        }
+    });
 
     const apolloServer = new ApolloServer({
         typeDefs: schema,
@@ -14,7 +23,8 @@ function gqlServer() {
         playground: true,
     });
 
-    apolloServer.applyMiddleware({app, path: '/', cors: true});
+    apolloServer.applyMiddleware({ app, path: '/', cors: true });
+
 
     return app;
 }
